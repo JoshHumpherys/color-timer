c09e29a79f0f6b3d931a2fef313c652efafab256
Josh Humpherys <joshua.humpherys@gmail.com>
2018-01-02 22:19:52 -0800

    Add scramble types

--- src/App.js
+++ src/App.js
@@ -1,14 +1,59 @@
 import React, { Component } from 'react'
 import Scrambo from 'scrambo'
 
+const types = {
+  222: '2x2x2',
+  333: '3x3x3',
+  444: '4x4x4',
+  555: '5x5x5',
+  666: '6x6x6',
+  777: '7x7x7',
+  clock: 'Clock',
+  minx: 'Megaminx',
+  pryam: 'Pyraminx',
+  sq1: 'Square 1',
+  skewb: 'Skewb'
+};
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    const scrambo = new Scrambo();
+
+    this.state = {
+      scrambo,
+      type: '333',
+      scramble: scrambo.get()[0]
+    };
+
+    this.generateScramble = this.generateScramble.bind(this);
+  }
+
+  changeType(type) {
+    this.setState({ type });
+  }
+
+  generateScramble(type) {
+    this.setState({ scramble: this.state.scrambo.type(type).get() });
+  }
+
   render() {
     return (
       <div className="app">
         <header className="header">
-          <h1 className="title">Cube Timer</h1>
+          <h1 className="scramble">{this.state.scramble}</h1>
         </header>
-        {new Scrambo().get(5).map(scramble => <p>{scramble}</p>)}
+        <br />
+        <select onChange={e => this.generateScramble(e.target.value)}>
+          {
+            Object.keys(types).map(key =>
+              <option key={key} value={key} selected={this.state.type === key ? 'selected' : ''}>
+                {types[key]}
+              </option>
+            )
+          }
+        </select>
       </div>
     );
   }
